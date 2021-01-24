d6974f096aba2cbbb608a39b1b8ecbcabbfc0937
limtae <dkslsdkdl@naver.com>
2021-01-24 15:07:36 +0900

    Add Account Controller

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
+const accountController = require('accounts/account.controller');
 
 //DB Connection
 require('_middleware/db');
@@ -27,10 +28,12 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
-
+//Routing
+app.use('/account', accountController);
 
 // global Error Handler
 app.use(errorHandler);
+
 
 const port = process.env.PORT;
 
